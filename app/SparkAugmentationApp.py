2fe6b4e98bec1f39e3ff64306991ece0fe0e833e
srinivas <srinivasu.matta@gmail.com>
2021-10-06 10:38:03 +0100

    added initial version of spark augmentation application files.

--- app/SparkAugmentationApp.py
+++ app/SparkAugmentationApp.py
@@ -1,5 +1,7 @@
+
 from app import ImageUtils
 import pyspark
+from app.autoaugment import CIFAR10Policy
 from pyspark.sql.types import *
 import os
 
@@ -31,30 +33,18 @@ if __name__ == '__main__':
     images_input_dir  = "/Users/srinivas/Desktop/github_projects/spark-augmentation-app/images"
     images_output_dir = "/Users/srinivas/Desktop/github_projects/spark-augmentation-app/images_processed"
 
-    images = spark.read.format("image").load(images_input_dir)
+    images_df = spark.read.format("image").load(images_input_dir)
 
 
-    def image_transform_mml():
-
-        img_transformer = ImageTransformer().setOutputCol("img_out").\
-            flip(flipCode = 1).\
-            resize(height = 200, width = 200).\
-            crop(0, 0, height = 180, width = 180)
-        transformed_images = img_transformer.transform(images).select("img_out").withColumnRenamed("img_out", "image")
-        df_new = transformed_images.withColumn('filenames',
-                                               regexp_replace(col("image.origin"), images_input_dir, images_output_dir))
-        df_new.write.format("org.apache.spark.ml.source.image.PatchedImageFileFormat").save(images_output_dir)
-
-
-    def image_transform_augmented(no_of_augmented_images):
+    def image_transform_augmented(images_df, no_of_augmented_images, image_schema, images_input_dir, images_output_dir):
 
         img_transformer = ImageTransformer().setOutputCol("out")
 
-        transformed_images = img_transformer.transform(images).select("out").withColumnRenamed("out", "image")
+        transformed_images = img_transformer.transform(images_df).select("out").withColumnRenamed("out", "image")
         transformed_df = transformed_images.select("image.height", "image.width", "image.nChannels", "image.mode", "image.origin", "image.data",
                                   "image.origin")
 
-        augement_imgs_df_new =transformed_df.rdd.mapPartitions(
+        augement_imgs_df_new = transformed_df.rdd.mapPartitions(
             lambda row_img_data_itr: ImageUtils.augment_image_generator(row_img_data_itr, no_of_augmented_images)).flatMap(lambda x: x).toDF(image_schema)
 
         augement_imgs_transformed_df = augement_imgs_df_new.withColumn('filenames',
@@ -62,8 +52,8 @@ if __name__ == '__main__':
         return augement_imgs_transformed_df
 
 
-    no_of_augmented_images = 5
-    augement_imgs_transformed_df = image_transform_augmented(no_of_augmented_images)
+    no_of_augmented_images = 6
+    augement_imgs_transformed_df = image_transform_augmented(images_df, no_of_augmented_images, image_schema, images_input_dir, images_output_dir)
     augement_imgs_transformed_df.write.format("org.apache.spark.ml.source.image.PatchedImageFileFormat")\
         .mode("overwrite").save(images_output_dir)
 
